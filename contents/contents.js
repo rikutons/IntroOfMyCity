66b085b87b62d5a62a9fce6138ad51d32904fc6c
Rikuto Sueta <rikutonsicloud@gmail.com>
2020-05-23 23:07:31 +0900

    Change to Sass(.sass) and some refactoring

--- contents/contents.js
+++ contents/contents.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.scroll').on('click', function(e) {
+  $('.contents__scroll').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'easeOutCubic');
   });
